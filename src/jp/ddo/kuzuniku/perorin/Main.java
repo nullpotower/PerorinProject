48245e02ff64de95cbbd3b391bdb8cc14eba8a74
Kuzuniku <kuzuniku@gmail.com>
2012-05-24 06:02:06 +0900

    最初のコミットです

---
+++ src/jp/ddo/kuzuniku/perorin/Main.java
@@ -0,0 +1,14 @@
+package jp.ddo.kuzuniku.perorin;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Perorin! now testing...");
+		// hello GitHub!
+
+	}
+
+}
