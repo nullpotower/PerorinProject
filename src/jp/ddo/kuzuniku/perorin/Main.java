e2faf29708768d5472f543b88c960b737d3390ec
nullpotower <cbr06600@hotmail.com>
2012-05-24 06:56:13 +0900

    ブラウザからkuzunikuのファイルに直接変更を加えるテスト

--- src/jp/ddo/kuzuniku/perorin/Main.java
+++ src/jp/ddo/kuzuniku/perorin/Main.java
@@ -9,6 +9,20 @@ public class Main {
 		System.out.println("Perorin! now testing...");
 		// hello GitHub!
 
+/*
+　　　　　　　　　　　　　　 ,, -―-、　　　　　　　
+　　　　　　　　　　　　　／　　　　 ヽ　　　
+　　　　　　　／￣￣／　　／i⌒ヽ､|　　　　オエーー！！！！
+　　　　　　/　　（゜）/　　 ／　/　　 　　　　　　　
+　　　　　/　　　　 ト､.,../　,ー-､　　　　　 　
+　　　　=彳　　　　　 ＼＼‘ﾟ。､｀ ヽ。、ｏ　 　
+　　　　/ 　 　　　　　　　＼＼ﾟ。､。、ｏ
+　　　/　　　　　　　　　/⌒ ヽ ヽU　　ｏ
+　　 /　　　　　　　　　│　　　`ヽU　∴ｌ
+　　│　　　　　　　　　│　　　　　U　：l
+　　　　　　　　　　　　　　　　　　　　|：!
+　　　　　　　　　　　　　　　　　　　　Ｕ 
+*/
 	}
 
 }
